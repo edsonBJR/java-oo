13eb04ea389ecea4fa833ce15fa2406af3d98ec2
edsonBJR <ebjr.tux@gmail.com>
2021-02-25 12:48:31 -0300

    Trabalhando com colecoes - listas.

---
+++ gerenciador-de-cursos/src/br/com/alura/Curso.java
@@ -0,0 +1,51 @@
+package br.com.alura;
+
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Curso {
+	
+	private String nome;
+	private String instrutor;
+	private List<Aula> aulas = new LinkedList<Aula>();
+	
+	public Curso(String nome, String instrutor) {
+		super();
+		this.nome = nome;
+		this.instrutor = instrutor;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+	
+	public String getInstrutor() {
+		return instrutor;
+	}
+	
+	public List<Aula> getAulas() {
+		return Collections.unmodifiableList(aulas);
+	}
+	
+	public void adiciona(Aula aula) {
+		this.aulas.add(aula);
+	}
+	
+	public int getTempoTotal() {
+		
+		return this.aulas.stream().mapToInt(Aula::getTempo).sum();
+		
+//		O código abaixo pode ser escrito com uma linha apenas, a linha de cima utilizando recursos do java 8.
+//		int tempoTotal = 0;
+//		for (Aula aula : aulas) {
+//			tempoTotal += aula.getTempo();
+//		}
+//		return tempoTotal;
+	}
+
+	@Override
+	public String toString() {
+		return "[Curso: " + nome + ", tempo total: " + this.getTempoTotal() + ", aulas" + this.aulas + "]";
+	}
+}
